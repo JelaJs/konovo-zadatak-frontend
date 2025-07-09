7824a2fd41f491d09025510e2cd3ac6c3df911f8
JelaJs <nikola4434@gmail.com>
2025-07-09 15:32:53 +0200

    Check if the user is logged in

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,4 +4,16 @@
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  const token = localStorage.getItem('jwt_token')
+  if (token) {
+    router.push({ name: 'login' })
+  }
+})
+</script>
