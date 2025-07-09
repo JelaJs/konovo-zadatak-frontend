43332e1db2d86bd3c4a2b43e63bcb76e64d9f857
JelaJs <nikola4434@gmail.com>
2025-07-09 15:10:52 +0200

    Redirecting to home if the user is already logged in

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,19 @@
-<script setup></script>
-
 <template>
   <main>
     <h1>Home Page</h1>
   </main>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  const token = localStorage.getItem('jwt_token')
+  if (token) {
+    router.push({ name: 'home' })
+  }
+})
+</script>
